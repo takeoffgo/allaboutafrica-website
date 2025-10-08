1f246a39dd0af8005896dd11ccb83e5610ea30b5
Brendan McKenzie <brendan@takeoffgo.com>
2025-10-08 11:19:18 +1100

    update landing page

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [{ hostname: "cdn.takeoffgo.com" }],
+  },
   /* config options here */
 };
 
