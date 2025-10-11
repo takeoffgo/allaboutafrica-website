c013bc3db9526902d824afdf5d2a0dea0c3666e1
Brendan McKenzie <brendan@takeoffgo.com>
2025-10-11 13:44:33 +1100

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,6 @@ import style from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      <div className={style.copy}>
-        <h1>
-          All About Africa
-          <br />
-          All Heart
-          <br />
-          All Coming Soon
-        </h1>
-      </div>
       <Image
         className={style.image}
         src="https://cdn.takeoffgo.com/808c2f5788f686b0d3ee4160f8a12dbb"
@@ -21,6 +12,25 @@ export default function Home() {
         width={5567}
         height={3711}
       />
+      <div className={style.copy}>
+        <h1>
+          All About Africa
+          <br />
+          All Heart
+          <br />
+          All Coming Soon
+          <br />
+          For the Travel Trade
+        </h1>
+      </div>
+      <div className={style.enquire}>
+        <p>Interested about partnering with us?</p>
+        <p>
+          <a href="mailto:info@allaboutafrica.au?subject=Tell me more about All About Africa">
+            Let&apos;s Talk Africa
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
