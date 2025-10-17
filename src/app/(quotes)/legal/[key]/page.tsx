8e4fff4fec08dea5197fb5a8f61f604473638d30
Brendan McKenzie <brendan@takeoffgo.com>
2025-10-17 13:23:55 +1100

    remove export

--- src/app/(quotes)/legal/[key]/page.tsx
+++ src/app/(quotes)/legal/[key]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound } from "next/navigation";
 import LegalDocumentPage from "../doc/page";
 
-export const knownLegalDocs: { [key: string]: string } = {
+const knownLegalDocs: { [key: string]: string } = {
   "privacy-policy": "b36aca91-20cc-4b81-aa0d-337d4f5cf963",
   "website-terms": "29bc0110-5953-4506-bcbb-2d0e4611f6fb",
   "booking-terms": "35e2fba7-3d36-4a3b-9dd0-73c07a6f76af",
