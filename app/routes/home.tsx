23a0c02c3d718b159cdaab93752b5785d739c0bc
Brendan McKenzie <brendan@takeoffgo.com>
2026-02-27 12:22:41 +0800

    clean up home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,7 @@
+import type { LinksFunction } from "react-router";
 import type { Route } from "./+types/home";
+
+import '../app.css';
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "All About Africa" }];
@@ -6,6 +9,37 @@ export function meta({}: Route.MetaArgs) {
 
 export function loader({}: Route.LoaderArgs) {}
 
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: "https://cdn.allaboutafrica.au/fonts/copernicus/font.css",
+  },
+];
+
 export default function Home({}: Route.ComponentProps) {
-  return <pre>Shhh... we're sleeping. Come back soon.</pre>;
+  return (
+    <>
+      <h1>
+        All About Africa
+        <br />
+        All Heart
+        <br />
+        All Coming Soon
+        <br />
+        For the Travel Trade
+      </h1>
+      <div>
+        <p>Interested about partnering with us?</p>
+        <p>
+          <a
+            href="https://rqo13.share.hsforms.com/2ZrXw0pG4TASFD_3lUpditw"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Let&apos;s Talk Africa
+          </a>
+        </p>
+      </div>
+    </>
+  );
 }
